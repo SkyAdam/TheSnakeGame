cde1189349887e594b15d153e00165a26d1242ec
SkyAdam <xander96ale@gmail.com>
2020-11-17 23:09:44 +0100

    dokonczenie gameplayu

--- TheSnake/Form1.Designer.cs
+++ TheSnake/Form1.Designer.cs
@@ -125,7 +125,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "SnakeGame";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.menuStrip1.ResumeLayout(false);
